2f64c1b5ea2076be75659c86b2c4517731b49a55
Phodal HUANG <h@phodal.com>
2018-02-01 22:05:59 +0800

    [T] add page track

--- src/app/decorators/PageTrack.ts
+++ src/app/decorators/PageTrack.ts
@@ -1,21 +1,26 @@
+import {AnalyticsService} from '../services/analytics.services';
+
 export function PageTrack(pageName: string): ClassDecorator {
   return function (constructor: any) {
-    console.log(pageName);
-    // const analyticsService = AppModule.injector.get(AnalyticsService);
-    //
-    // const ngOnInit = constructor.prototype.ngOnInit;
-    //
-    // constructor.prototype.ngOnInit = function (...args) {
-    //   analyticsService.visit(pageName);
-    //   ngOnInit && ngOnInit.apply(this, args);
-    // };
-    //
-    // const ngOnDestroy = constructor.prototype.ngOnDestroy;
-    //
-    // constructor.prototype.ngOnDestroy = function (...args) {
-    //   analyticsService.leave(pageName);
-    //   ngOnDestroy && ngOnDestroy.apply(this, args);
-    // };
+    const analyticsService = new AnalyticsService();
+
+    const ngOnInit = constructor.prototype.ngOnInit;
+
+    constructor.prototype.ngOnInit = function (...args) {
+      analyticsService.visit(pageName);
+      if (ngOnInit) {
+        ngOnInit.apply(this, args);
+      }
+    };
+
+    const ngOnDestroy = constructor.prototype.ngOnDestroy;
+
+    constructor.prototype.ngOnDestroy = function (...args) {
+      analyticsService.leave(pageName);
+      if (ngOnDestroy) {
+        ngOnDestroy.apply(this, args);
+      }
+    };
 
   };
 }
--- src/app/services/analytics.services.ts
+++ src/app/services/analytics.services.ts
@@ -1,9 +1,9 @@
 export class AnalyticsService {
   visit(pageName: string) {
-
+    console.log('visit' + pageName);
   }
 
   leave(pageName: string) {
-
+    console.log('leave' + pageName);
   }
 }
