e54c646e885994e97250b3953e4d987c49dec652
Phodal HUANG <h@phodal.com>
2018-02-06 23:21:41 +0800

    [T] thinking in role permission

--- src/app/pages/home-work/home-work.component.ts
+++ src/app/pages/home-work/home-work.component.ts
@@ -3,6 +3,11 @@ import {PageTrack} from '../../decorators/PageTrack';
 import {Role} from '../../decorators/RoleDecorator';
 import {AuthService} from '../../services/auth.service';
 
+
+class PermissionComponent {
+
+}
+
 @Component({
   selector: 'app-home-work',
   templateUrl: './home-work.component.html',
@@ -11,11 +16,12 @@ import {AuthService} from '../../services/auth.service';
 
 @Role({type: 'action'})
 @PageTrack('HomeWorkComponent')
-export class HomeWorkComponent implements OnInit {
+export class HomeWorkComponent extends PermissionComponent implements OnInit {
   private authService: AuthService;
   private role: any;
 
   constructor(authService: AuthService) {
+    super();
     this.authService = authService;
   }
 
