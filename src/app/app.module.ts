9457ec7a7449779ad6737938331da0899e67ea00
Phodal HUANG <h@phodal.com>
2018-02-28 16:42:55 +0800

    [T] remove unused import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,8 +17,6 @@ import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {ModelComponent} from './components/ModelComponent';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {RouterModule} from '@angular/router';
-import { RoleMenuComponent } from './shared/role-menu/role-menu.component';
-
 
 @NgModule({
   declarations: [
