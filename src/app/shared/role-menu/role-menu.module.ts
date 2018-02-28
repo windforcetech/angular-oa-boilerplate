7b5687df0cb82833d0787929ac4b50939d30341e
Phodal HUANG <h@phodal.com>
2018-02-28 16:48:19 +0800

    [T] remove unsued modules

--- src/app/shared/role-menu/role-menu.module.ts
+++
@@ -1,9 +0,0 @@
-import {NgModule} from '@angular/core';
-import {RoleMenuComponent} from './role-menu.component';
-
-@NgModule({
-  declarations: [RoleMenuComponent],
-  exports: [RoleMenuComponent]
-})
-export class RoleMenuModule {
-}
