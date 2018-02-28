695709436e627e8830854ac425116928592d4ca0
Phodal HUANG <h@phodal.com>
2018-02-28 20:58:49 +0800

    [LOG] create basic function

--- src/app/services/logger.services.ts
+++ src/app/services/logger.services.ts
@@ -1,3 +1,6 @@
+import {Injectable} from '@angular/core';
+import {environment} from '../../environments/environment';
+
 export abstract class Logger {
   off: any;
   error: any;
@@ -5,4 +8,34 @@ export abstract class Logger {
   info: any;
   debug: any;
   all: any;
+}
+
+const noop = (): any => undefined;
+export let isDebugMode = environment.production;
+
+@Injectable()
+export class LoggerService implements Logger {
+  get off() {
+    return noop;
+  }
+
+  get error() {
+    return noop;
+  }
+
+  get warn() {
+    return noop;
+  }
+
+  get info() {
+    return noop;
+  }
+
+  get debug() {
+    return noop;
+  }
+
+  get all() {
+    return noop;
+  }
 }
