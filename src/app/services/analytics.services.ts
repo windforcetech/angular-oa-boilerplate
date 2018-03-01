a1ab4157052b238ddb3e605fe5098c220dd170d9
Phodal HUANG <h@phodal.com>
2018-03-01 11:10:22 +0800

    [LOGGER] add append log behavior

--- src/app/services/analytics.services.ts
+++ src/app/services/analytics.services.ts
@@ -1,23 +1,39 @@
 import {LoggerService} from './logger.services';
 import {Injectable} from '@angular/core';
 
+function appendToStorage(name, data) {
+  let old = localStorage.getItem(name);
+  if (old === null) {
+    old = '';
+  } else {
+    old = old + ',';
+  }
+  localStorage.setItem(name, old + data);
+}
+
 @Injectable()
 export class AnalyticsService {
   private logger: LoggerService = new LoggerService();
 
   visit(pageName: string) {
-    this.logger.log({
+    const message = {
       behavior: 'enter',
       date: new Date(),
-      pageName: pageName
-    });
+      pageName: pageName,
+      url: window.location.href,
+    };
+    this.logger.log(message);
+    appendToStorage('page', JSON.stringify(message));
   }
 
   leave(pageName: string) {
-    this.logger.log({
+    const message = {
       behavior: 'leave',
       date: new Date(),
-      pageName: pageName
-    });
+      pageName: pageName,
+      url: window.location.href,
+    };
+    this.logger.log(message);
+    appendToStorage('page', JSON.stringify(message));
   }
 }
