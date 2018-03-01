bd41429256a823741682389b34a42a899440c631
Phodal HUANG <h@phodal.com>
2018-03-01 10:05:41 +0800

    [LOGGER] log global user click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, ViewContainerRef} from '@angular/core';
+import {Component, HostListener, OnInit, ViewContainerRef} from '@angular/core';
 import {RebirthNGConfig} from 'rebirth-ng';
 
 @Component({
@@ -7,6 +7,11 @@ import {RebirthNGConfig} from 'rebirth-ng';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  @HostListener('document:click', ['$event'])
+  onDocumentClicked(ev) {
+    console.log('clicked', ev);
+  }
+
   constructor(private rebirthNGConfig: RebirthNGConfig,
               private viewContainerRef: ViewContainerRef) {
 
