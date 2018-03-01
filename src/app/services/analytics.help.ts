93dfdc7677b00f96bdc2e0591c5ccc7973e3de00
Phodal HUANG <h@phodal.com>
2018-03-01 10:29:00 +0800

    [log] add convert json

--- src/app/services/analytics.help.ts
+++ src/app/services/analytics.help.ts
@@ -1,6 +1,34 @@
+const convertJSON = function (obj) {
+  const o = {
+    o: null
+  };
+  o.o = obj;
+
+  let cache = [];
+  JSON.stringify(o, function(key, value) {
+    if (typeof value === 'object' && value !== null) {
+      if (cache.indexOf(value) !== -1) {
+        return;
+      }
+      cache.push(value);
+    }
+
+    return value;
+  });
+  cache = null;
+
+  return o.o;
+};
+
 const AnalyticsHelper = {
   ClickLogger: function (mouseEvent) {
     console.log(mouseEvent);
+    const convertJson = convertJSON(mouseEvent);
+    console.log({
+      path: convertJson.path
+    });
+
+    localStorage.setItem('log', convertJson.path);
   }
 };
 
